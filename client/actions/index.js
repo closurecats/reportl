bdb89b5b43cc172ecfc69af48de2fb36dedcb946
Sam Phillips <armyf35@gmail.com>
2017-01-03 14:29:24 -0500

    Fixed loginSubmit action url

--- client/actions/index.js
+++ client/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function loginSubmit(login) {
-  const request = axios.post('/login', login);
+  const request = axios.post('/auth/login', login);
   return {
     type: 'LOGIN_SUBMITTED',
     payload: request,
