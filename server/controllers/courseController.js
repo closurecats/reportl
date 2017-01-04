17c3cad26a70e5dbda4014fcc9282c26bed97e6b
Sam Phillips <armyf35@gmail.com>
2017-01-03 19:56:46 -0500

    Added course updateCourseById

--- server/controllers/courseController.js
+++ server/controllers/courseController.js
@@ -61,6 +61,20 @@ const courseController = {
         res.sendStatus(500);
       });
   },
+
+  updateCourseById({ params: { id }, body: courseData }, res) {
+    Course.forge({ id })
+      .fetch()
+      .then((course) => {
+        Object.keys(courseData).forEach(key => course.set(key, courseData[key]));
+        return course.save();
+      })
+      .then(course => res.json(course))
+      .catch((err) => {
+        console.log(`courseController.updateCourseById - Error: ${err}`);
+        res.sendStatus(500);
+      });
+  },
 };
 
 module.exports = courseController;
--- server/routers/courseRouter.js
+++ server/routers/courseRouter.js
@@ -4,6 +4,7 @@ const courseController = require('../controllers/courseController');
 router.get('/', courseController.getAll);
 router.get('/:id', courseController.getCourseById);
 router.put('/:id', courseController.updateCourseById);
+router.delete('/:id', courseController.deleteCourseById);
 router.get('/:id/classes', courseController.getAllClass);
 router.post('/', courseController.newCourse);
 
