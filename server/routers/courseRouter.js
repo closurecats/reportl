1e82088565ba6fc97b6ed63d7d34b5166030a021
Sam Phillips <armyf35@gmail.com>
2017-01-03 19:55:37 -0500

    Moved course getAllClass to /:id/classes

--- server/routers/courseRouter.js
+++ server/routers/courseRouter.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const courseController = require('../controllers/courseController');
 
 // router.get('/:id', courseController.getCourseById);
-router.get('/:id', courseController.getAllClass);
 router.get('/', courseController.getAll);
+router.get('/:id/classes', courseController.getAllClass);
 router.post('/', courseController.newCourse);
 
 module.exports = router;
