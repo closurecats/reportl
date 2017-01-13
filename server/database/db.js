9f3c057e5f7daab9b8aa724e2b7b639ed772ee8a
Sam Phillips <armyf35@gmail.com>
2017-01-13 15:55:58 -0500

    Removed unused variable

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,6 @@
 const knexConfig = require('./knexfile');
 const knex = require('knex')(knexConfig[process.env.NODE_ENV || 'development']);
 const db = require('bookshelf')(knex);
-const exec = require('child_process').exec;
 
 db.plugin('visibility');
 db.plugin('virtuals');
