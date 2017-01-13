572a800088ae999a19ac17e1b300e67a41c31eb2
Chris Stair <ccstair@gmail.com>
2017-01-13 14:48:02 -0500

    Styled setup form, added old hero image

--- client/containers/setup_school_forms/render_setup_form.jsx
+++ client/containers/setup_school_forms/render_setup_form.jsx
@@ -1,12 +1,51 @@
 import React from 'react';
-import SetupFormList from './setup_form_list';
-import SetupFormDetail from './setup_form_detail';
+import UserFormMaker from '../addUser';
+import DepartmentFormMaker from '../addDepartment';
+import CourseFormMaker from '../addCourse';
 
-const RenderSetupForm = () => (
-  <div>
-    <SetupFormList />
-    <SetupFormDetail />
-  </div>
-);
+class RenderSetupForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.switchView = this.switchView.bind(this);
+    this.state = {
+      viewNow: 'User',
+    };
+  }
+
+  switchView(viewName) {
+    this.setState({ viewNow: viewName });
+  }
+
+  currentView() {
+    if (this.state.viewNow === 'User') {
+      return <UserFormMaker />;
+    }
+
+    if (this.state.viewNow === 'Department') {
+      return <DepartmentFormMaker />;
+    }
+
+    if (this.state.viewNow === 'Course') {
+      return <CourseFormMaker />;
+    }
+
+    return false;
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="viewSwitch">
+          <button className="userViewButton" onClick={() => this.switchView('User')}>Add User</button>
+          <button className="userViewButton" onClick={() => this.switchView('Department')}>Add Department</button>
+          <button className="userViewButton" onClick={() => this.switchView('Course')}>Add Course</button>
+        </div>
+        <div className="formContain">
+          {this.currentView()}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default RenderSetupForm;
